e5758fb436b761bf4aaae1e728c9ac37a2960a2e
Your NameIcksDeh <diagnose-neongruen@gmx.de>
2025-08-08 16:19:13 +0200

    added shadow effect to cart, fixed issue with database.js

--- scripts/database.js
+++ scripts/database.js
@@ -3,18 +3,18 @@ let mainDishes = [
         "name":"Gebratene Nudeln mit Gemüse und Ei",
         "price":8.00,
         "discription":"Mit frischem Gemüse und Soya Soße",
-        "amount":0
-     }
+        "amount":0,
+     },
      {
         "name":"Sommerrollen",
         "price":14.00,
         "discription":"3 Sommerrollen, Gefüllt mit Gemüse und Hähnchen",
-        "amount":0
-     }
+        "amount":0,
+     },
      {
         "name":"Reis mit gebratenem ToFu und Gemüse",
         "price":18.00,
         "discription":"Mit frischem Gemüse, Leicht scharf",
-        "amount":0
+        "amount":0,
      }
 ]
