c0d25f975cf1e2aee0adb194bcd589cbabfdee66
Your NameIcksDeh <diagnose-neongruen@gmx.de>
2025-08-09 13:18:14 +0200

    add all maindishes to cart, raise amount in cart by clicking again on maindish

--- scripts/script.js
+++ scripts/script.js
@@ -11,4 +11,35 @@ function renderMainDish(){
     }
 }
 
+function addMainDishToCart(mainDishIndex){
+    mainDishes[mainDishIndex].amount += 1;
+    showMainDishInCart();
+}
+
+function reduceMainDishInCart(mainDishIndex){
+    mainDishes[mainDishIndex].amount -= 1;
+    showMainDishInCart();
+}
+
+function showMainDishInCart(){
+    let showCartContent = document.getElementById('cart_content_wrapper');
+    showCartContent.innerHTML = "";
+    for (let index = 0; index < mainDishes.length; index++) {
+        if(mainDishes[index].amount >= 1){
+            showCartContent.innerHTML += showMainDishInCartTemplate(index);
+        }; 
+    }
+}
+
+function raiseDishInCart(mainDishIndex){
+    mainDishes[mainDishIndex].amount += 1;
+    renderAmount(mainDishIndex);
+}
+
+function renderAmount(mainDishIndex) {
+    let mainDishInCart = document.getElementById('main_dish_in_cart'+ mainDishIndex);
+    mainDishInCart.innerHTML= renderMainDishInCartTemplate(mainDishIndex);
+    
+}
+
 
--- scripts/template.js
+++ scripts/template.js
@@ -6,7 +6,40 @@ function mainDishTemplate(mainDishIndex) {
                         <p id="price_main_dish ${mainDishIndex}" class="price_style">Preis ${mainDishes[mainDishIndex].price.toFixed(2)}€</p>
                     </div>
                     <div class="div_add_button">
-                        <button id="add_main_dish ${mainDishIndex}" class="add_button">+</button>
+                        <button onclick="addMainDishToCart(${mainDishIndex})" ${mainDishIndex}" class="add_button">+</button>
                     </div>
                 </section>`      
+}
+
+function showMainDishInCartTemplate(mainDishIndex){
+    return `<div class="cart_content id="cart_content_id ${mainDishIndex}">
+                <h3 class="title_basket_style">${mainDishes[mainDishIndex].name}</h3>
+                <div class="orderelements_in_cart">
+                    <div id="main_dish_in_cart ${mainDishIndex}" class="button_elements">
+                        <button class="button_cart">
+                            <img class="button_cart_plus_minus" src="./assets/img/minus_PP.svg" alt="Weniger">
+                        </button>
+                        <p>${mainDishes[mainDishIndex].amount}x</p>
+                        <button onclick="raiseDishInCart(${mainDishIndex})  class="button_cart">
+                            <img class="button_cart_plus_minus" src="./assets/img/plus_PP.svg" alt="Mehr">
+                        </button>
+                    </div>
+                    <p>${mainDishes[mainDishIndex].price.toFixed(2)}€</p>
+                    <Button class="button_cart">
+                        <img class="button_cart_trash" src="./assets/img/trash_2_PP.svg" alt="Entfernen">
+                    </Button>
+                </div>
+    
+    `
+}
+function renderMainDishInCartTemplate(mainDishIndex){
+    return `<button class="button_cart">
+                <img class="button_cart_plus_minus" src="./assets/img/minus_PP.svg" alt="Weniger">
+            </button>
+            <p>${mainDishes[mainDishIndex].amount}x</p>
+            <button onclick="raiseDishInCart(${mainDishIndex})  class="button_cart">
+            <img class="button_cart_plus_minus" src="./assets/img/plus_PP.svg" alt="Mehr">
+            </button>
+    
+    `
 }
