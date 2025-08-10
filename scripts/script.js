a40a2a74427bc73aed5cccdb47aa9591533af985
Your NameIcksDeh <diagnose-neongruen@gmx.de>
2025-08-10 13:41:07 +0200

    finshed calculation subtotal and total costs, toggle view cart

--- scripts/script.js
+++ scripts/script.js
@@ -32,7 +32,7 @@ function showMainDishInCart(){
             showCartContent.innerHTML += showMainDishInCartTemplate(index);
         }; 
     }
-    calculateSubtotal();
+    calculateSubtotalCosts();
 }
 
 function setMainDishToZero(mainDishIndex){
@@ -41,16 +41,34 @@ function setMainDishToZero(mainDishIndex){
 
 }
 
-function calculateSubtotal(){
+function calculateSubtotalCosts(){
+    let showSubtotalCosts = document.getElementById('show_subtotal');
     let subtotal = 0;
     for (let index = 0; index < mainDishes.length; index++) {
        let resultEachDish = mainDishes[index].amount * mainDishes[index].price;
        subtotal += resultEachDish;
-       console.log(subtotal);
-       resultEachDish.innerHTML = showSubtotalTemplate();
-        
+       showSubtotalCosts.innerHTML = subtotal.toFixed(2)+"€"; 
+    }
+    calculateTotalCosts(subtotal);
+}
+
+function calculateTotalCosts(subtotal){
+    let showTotalCosts = document.getElementById('show_total_costs');
+    let deliveryCosts = document.getElementById('delivery_costs');
+    let totalCosts = 0
+    if(subtotal >= 20.00){
+        deliveryCosts.innerHTML = "0,00€";
+        showTotalCosts.innerHTML = subtotal.toFixed(2) + "€";
+    }
+    else {
+        deliveryCosts.innerHTML = "5,00€";
+        totalCosts = subtotal + 5;
+        showTotalCosts.innerHTML = totalCosts.toFixed(2)+'€';
     }
 }
 
 function toggleCart(){
+    document.getElementById('id_content_main_wrapper').classList.toggle('content_main_wrapper');
+    document.getElementById('id_main_cart_wrapper').classList.toggle('main_cart_wrapper');
+    document.getElementById('id_cartwrapper').classList.toggle('d_none');
 }
